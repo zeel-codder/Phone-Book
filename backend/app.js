afe8af65570298693295e81eb33485e0a0fe581e
zeel-codder <zeelprajapati321@gmail.com>
2022-06-27 11:48:33 +0530

    init

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+var createError = require("http-errors");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var phoneRouter = require("./routes/phone");
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//routers
+app.use("/phone", phoneRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+module.exports = app;
